d1bd53c492f6f6f7ece624f969a34648ad97c93b
Ernalene Roocha <ernalene_rocha@dlsl.edu.ph>
2025-08-08 14:00:18 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Thank you for contacting me!");
+}
+
+function changePortfolioBg() {
+  const section = document.getElementById("projects");
+  section.style.backgroundColor = "#f0f8ff";
+}
